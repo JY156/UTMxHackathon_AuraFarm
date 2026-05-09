40bd3b2683a4c91df87a6231763d5d269f3a6184
nicolheng <nicolhengsiyi@gmail.com>
2026-05-09 22:07:45 +0800

    move humidity mist from irrigation to mist system

--- frontend/src/features/farm/FarmScene.tsx
+++ frontend/src/features/farm/FarmScene.tsx
@@ -288,7 +288,7 @@ function FarmScene({ sensors, actuators, alerts, profile }: FarmSceneProps) {
           />
 
           {/* Humidity Mist effect */}
-          <HumidityMist active={actuators.pump || sensors.humidity > 70} position={[0, 0, 3]} />
+          <HumidityMist active={actuators.mist || sensors.humidity > 70} position={[0, 0, 3]} />
 
           <SelectToFocus id="control-box">
             <ControlBox position={[1.9, 3, 7]} rotation={[0, -Math.PI / 2, 0]} />
